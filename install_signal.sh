27354f1bece023b0b9c0829ed92f2d107190eb71
vk <vinay.rvce@gmail.com>
2021-03-04 12:00:36 +0100

    added docker and signal installation scripts

---
+++ install_signal.sh
@@ -0,0 +1,11 @@
+echo "Installing Signal-desktop"
+
+wget -O- https://updates.signal.org/desktop/apt/keys.asc |\
+  sudo apt-key add -
+
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" |\
+  sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+sudo apt update && sudo apt install signal-desktop
+
+echo "Installation script completed for signal-desktop!!"
