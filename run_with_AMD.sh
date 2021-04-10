16fc6672a78154b6fd34d7e54a81502845e5dd66
vk <vinay.rvce@gmail.com>
2021-04-10 14:40:05 +0200

    added vulkan libs

--- run_with_AMD.sh
+++ run_with_AMD.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/radeon_icd.i686.json:/usr/share/vulkan/icd.d/radeon_icd.x86_64.json
 export __NV_PRIME_RENDER_OFFLOAD=0;
 export __GLX_VENDOR_LIBRARY_NAME=amdgpu;
 export DRI_PRIME=0
--- run_with_NVDIA.sh
+++ run_with_NVDIA.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json
 export __NV_PRIME_RENDER_OFFLOAD=1;
 export __GLX_VENDOR_LIBRARY_NAME=nvidia;
 export DRI_PRIME=0;
